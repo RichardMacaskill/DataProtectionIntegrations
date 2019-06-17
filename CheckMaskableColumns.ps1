b85881b656af3bc7da107007973944ae935d3fc7
Richard Macaskill <richard.macaskill@red-gate.com>
2019-06-17 09:58:31 +0100

    Update CheckMaskableColumns.ps1

--- CheckMaskableColumns.ps1
+++ CheckMaskableColumns.ps1
@@ -1,10 +1,11 @@
-Invoke-WebRequest -Uri 'https://rm-win10-sql201.testnet.red-gate.com:15156/powershell' -OutFile 'data-catalog.psm1' -Headers @{"Authorization"="Bearer NTM2OTUxMTYyNzA4OTUxMDQwOmRiNjIyYWMxLWI1NDYtNDQzNi04OTE2LWQ1MzkxNGIzYzI5MQ=="}
+$authToken = "NTM2OTUxMTYyNzA4OTUxMDQwOmRiNjIyYWMxLWI1NDYtNDQzNi04OTE2LWQ1MzkxNGIzYzI5MQ=="
+
+Invoke-WebRequest -Uri 'https://rm-win10-sql201.testnet.red-gate.com:15156/powershell' -OutFile 'data-catalog.psm1' -Headers @{"Authorization"="Bearer $authToken"}
  
-Import-Module .\data-catalog.psm1 -Force
+Import-Module .\data-catalog.psm1 
 
 $instanceName = 'rm-iclone1.testnet.red-gate.com'
 $databaseName = 'StackOverflow2010'
-$authToken = "NTM2OTUxMTYyNzA4OTUxMDQwOmRiNjIyYWMxLWI1NDYtNDQzNi04OTE2LWQ1MzkxNGIzYzI5MQ=="
 
 # connect to your SQL Data Catalog instance - you'll need to generate an auth token in the UI
 Use-Classification -ClassificationAuthToken $authToken 
@@ -14,6 +15,7 @@ $allColumns = Get-Columns -instanceName $instanceName -databaseName $databaseNam
 
 "Columns returned: "+  $allColumns.Count
 
+# Filter on sensitivity label text match for GDPR 
 $maskableColumns = $allColumns |  Where-Object {$_.sensitivityLabel -like  "*GDPR*"}
 
 $maskableColumns.schemaName | Select-Object -Unique    
